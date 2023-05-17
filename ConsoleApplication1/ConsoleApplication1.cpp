6a05b6a6be8c3dc0072097556a8a0e51b8bd10e6
artemshanga <mr.shan4321@gmail.com>
2023-05-17 10:40:12 +0200

    v1
    files added

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+
+using namespace std;
+void delitel(int number) {
+    
+    if (number > 2147483646) {
+        wcout << L"Помилка! Це число занадто велике!";
+    }else if(number < -2147483646){
+        wcout << L"Помилка! Це число занадто мале!";
+    }
+    else {
+    wcout << L"Дільники числа: ";
+    for (int i = 1; i <= number; i++) {
+        if (number % i == 0) {
+             wcout << i << " ";
+        }
+    }
+
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, ".1251");
+    int number;
+
+    std::wcout << L"Введите натуральное число: ";
+    std::cin >> number;
+
+     delitel(number);
+
+   
+}
